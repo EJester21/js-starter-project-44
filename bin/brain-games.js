69690653e178a73eb7e39c8f19418a1fe62d6bf3
Maxim <exclusivejester@gmail.com>
2023-02-17 02:03:28 +0500

    add is even game

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,7 +1,25 @@
 #!/usr/bin/env node
+import { greet, askName, greetUser, showTask, askQuestion } from '../src/cli.js';
 
-import askName from '../src/cli.js';
+const isEven = (num) => num % 2 === 0;
 
-console.log('Welcome to the Brain Games!');
-const name = askName();
-console.log(`Hello, ${name}!`);
+const playEvenGame = () => {
+  greet();
+  const name = askName();
+  greetUser(name);
+  showTask();
+  for (let i = 1; i <= 3; i += 1) {
+    const question = Math.floor(Math.random() * 100);
+    const answer = askQuestion(question);
+    if ((isEven(question) && answer === 'yes') || (!isEven(question) && answer === 'no')) {
+      console.log('Correct!');
+    } else {
+      console.log(`'${answer}' is wrong answer ;(. Correct answer was '${isEven(question) ? 'yes' : 'no'}'.`);
+      console.log(`Let's try again, ${name}!`);
+      return;
+    }
+  }
+  console.log(`Congratulations, ${name}!`);
+};
+
+playEvenGame();
